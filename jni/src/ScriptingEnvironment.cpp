c95ee34830f131d63e7228f0c8fe2cfd5b949872
Nicolas Gramlich <ngramlich@zynga.com>
2012-03-02 16:50:45 -0800

    ScriptingEnvironment is now holding a pointer to the JavaVM. Minor renaming.

--- jni/src/ScriptingEnvironment.cpp
+++ jni/src/ScriptingEnvironment.cpp
@@ -5,29 +5,32 @@
 // org.andengine.extension.scripting.ScriptingEnvironment
 // ===========================================================
 
-jobject mContext;
-JNIEnv* mJNIEnv;
-EngineProxy* mEngine;
+static jobject sContext;
+static JavaVM* sJavaVM;
+static JNIEnv* sJNIEnv;
+static EngineProxy* sEngine;
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* pJavaVM, void* pReserved) {
+	sJavaVM = pJavaVM;
+ 
 	return JNI_VERSION_1_4;
 }
 
 JNIEXPORT void JNICALL Java_org_andengine_extension_scripting_ScriptingEnvironment_nativeInit(JNIEnv* pJNIEnv, jclass pJClass, jobject pContext, jstring pAPKPath, jobject pEngine) {
-	mContext = pContext;
-	mJNIEnv = pJNIEnv;
+	sContext = pContext;
+	sJNIEnv = pJNIEnv;
 
 	setAPKPath(pJNIEnv, pJClass, pAPKPath);
 
-	mEngine = new EngineProxy(pEngine);
+	sEngine = new EngineProxy(pEngine);
 }
 
 JNIEnv* JNI_ENV() {
-	return mJNIEnv;
+	return sJNIEnv;
 }
 
 EngineProxy* getEngine() {
-	return mEngine;
+	return sEngine;
 }
 
 void setAPKPath(JNIEnv* pJNIEnv, jclass pJClass, jstring pAPKPath) {
